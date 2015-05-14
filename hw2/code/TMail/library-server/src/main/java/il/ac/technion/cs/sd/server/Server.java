bd896983b0273722467be3dbe3de63440ea5d641
software-design <software-design@OferDesktop1-ubuntu.(none)>
2015-05-15 01:43:47 +0300

    added initial server code (that was commited in 3204365)

---
+++ hw2/code/TMail/library-server/src/main/java/il/ac/technion/cs/sd/server/Server.java
@@ -0,0 +1,23 @@
+package il.ac.technion.cs.sd.server;
+
+public class Server {
+	
+	private TaskFactory _taskFactory;
+		
+	public Server(TaskFactory taskFactory)
+	{
+		_taskFactory = taskFactory;
+	}
+	
+	
+	void Listen()
+	{
+		Mesanger messanger();
+		String json = messanger.listen();
+		
+		Task task = _taskFactory.create(json);
+
+		task.run();
+		
+	}
+}
