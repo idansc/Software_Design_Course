c0bd92841ee23f3a40207fbbf7e64775d81371f4
Ofer Givoli <ogivoli@cs.technion.ac.il>
2015-05-15 05:44:09 +0300

    finished server library. No tests yet.

--- hw2/code/TMail/library-server/src/main/java/il/ac/technion/cs/sd/lib/clientserver/Server.java
+++ hw2/code/TMail/library-server/src/main/java/il/ac/technion/cs/sd/lib/clientserver/Server.java
@@ -1,27 +1,101 @@
 package il.ac.technion.cs.sd.lib.clientserver;
 
-import il.ac.technion.cs.sd.msg.Messenger;
+import java.util.Optional;
 
+
+import il.ac.technion.cs.sd.msg.Messenger;
+import il.ac.technion.cs.sd.msg.MessengerException;
+import il.ac.technion.cs.sd.msg.MessengerFactory;
+
+
+/* Represents a server that performs a given task whenever data is recived from 
+ * any client. */
 public class Server {
 	
+	private String _serverAddress;
 	private ServerTask _task;
 	
+	/* This is thread running the listening loop. 
+	 * It's null iff no listen loop is currently done, or stopListenLoop was
+	 * just called and the listen loop is going to stop. */ 
+	private Thread listenThread = null; 
+	
+	private boolean stopListenRequested = false;
+	
+	
+	public class NoCurrentListenLoop extends RuntimeException {private static final long serialVersionUID = 1L;} 
+	public class ListenLoopAlreadyBeingDone extends RuntimeException {private static final long serialVersionUID = 1L;} 
+	
+	
 	/* @param task The server task to be performed each time data is sent to the server. */
-	public Server(ServerTask task)
+	public Server(String serverAddress, ServerTask task)
 	{
+		_serverAddress = serverAddress;
 		_task = task;
 	}
 	
 	
-	public void listen()
+	/* Starts a "listen loop" in which the server repeatedly listens for data
+	 * from clients, and for each data package sent - the given task is run. 
+	 * This is done until stopListenLoop is called.
+	 * This function is non-blocking (everything is done on another thread).
+	 **/
+	public void startListenLoop()
 	{
-		Messenger messanger = new Messenger();
+		if (listenThread != null )
+		{
+			throw new ListenLoopAlreadyBeingDone();
+		}
+		stopListenRequested = false;
 		
-		String json = messanger.listen();
+		listenThread = new Thread( () -> {
+			try {
+				Messenger messenger = createMessenger();
+				while (!stopListenRequested)
+				{
+					Optional<byte[]> data = messenger.tryListen();
+					if (data.isPresent())
+					{
+						_task.run(this,data.get());
+					}
+	
+					// If we cared about not wasting CPU time:
+					// Thread.sleep(10);
+				}
+				messenger.kill();
+			} catch (MessengerException exc)
+			{
+				// hmmm, no indication is passed on, oh well :(
+			}
+				
+		});
 		
-		Task task = _taskFactory.create(json);
-
-		task.run();
+	
 		
+		listenThread = null;
+	}
+	
+	
+	public void stopListenLoop()
+	{
+		if (listenThread == null )
+		{
+			throw new NoCurrentListenLoop();
+		}
+		stopListenRequested = true;
+		listenThread = null;
+	}
+	
+	
+	public void sendDataToClient(String clientAddress, byte[] data) throws MessengerException
+	{
+		Messenger m = createMessenger();
+		m.send(clientAddress, data);
+		m.kill();
+	}
+	
+	
+	private Messenger createMessenger() throws MessengerException {
+		return (new MessengerFactory()).start(_serverAddress);
 	}
 }
--- hw2/code/TMail/library-server/src/main/java/il/ac/technion/cs/sd/lib/clientserver/ServerTask.java
+++ hw2/code/TMail/library-server/src/main/java/il/ac/technion/cs/sd/lib/clientserver/ServerTask.java
@@ -1,8 +1,12 @@
 package il.ac.technion.cs.sd.lib.clientserver;
 
+/* Represents a task a server performs repeatedly - each time new data is sent 
+ * from any client.*/
+@FunctionalInterface
 public interface ServerTask {
 	
-	/* @param sender The address of the host sending the request.
-	 * @param data The data sent to the server on which the task should act upon. */
-	void run(String sender, byte[] data);
+	/* @param server The server processing this task (it'll be the same server 
+	 * for all repeated calls to 'run' made with this object).
+	 * @param data The data that the client sent to the server. */
+	void run(Server server, byte[] data);
 }
