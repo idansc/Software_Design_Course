8a38aab0e9e425419e77864479e672042a7b65f1
idansc <idansc@LAP429.cs.technion.ac.il>
2015-05-15 22:43:41 +0300

    removed

--- hw2/code/TMail/library-common/src/main/java/il/ac/technion/cs/sd/lib/clientserver/AbstractMessage.java
+++ hw2/code/TMail/library-common/src/main/java/il/ac/technion/cs/sd/lib/clientserver/AbstractMessage.java
@@ -1,10 +1,12 @@
+/**
+ * 
+ */
 package il.ac.technion.cs.sd.lib.clientserver;
 
-// Represents a data packet one host is sending another.
-public abstract class AbstractMessage
-{
-	// Address of the host sending this data packet.
-	public String fromAddress;
-	
-}
+/**
+ * @author idansc
+ *
+ */
+public class AbstractMessage {
 
+}
