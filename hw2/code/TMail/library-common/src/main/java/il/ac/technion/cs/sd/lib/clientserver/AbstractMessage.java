a3ec1d48be37b59eab9b05005e63d134265062ba
idansc <idansc@LAP429.cs.technion.ac.il>
2015-05-15 21:56:17 +0300

    change to message

---
+++ hw2/code/TMail/library-common/src/main/java/il/ac/technion/cs/sd/lib/clientserver/AbstractMessage.java
@@ -0,0 +1,10 @@
+package il.ac.technion.cs.sd.lib.clientserver;
+
+// Represents a data packet one host is sending another.
+public abstract class AbstractMessage
+{
+	// Address of the host sending this data packet.
+	public String fromAddress;
+	
+}
+
--- hw2/code/TMail/library-common/src/main/java/il/ac/technion/cs/sd/lib/clientserver/ServerTaskEndedMessage.java
+++ hw2/code/TMail/library-common/src/main/java/il/ac/technion/cs/sd/lib/clientserver/ServerTaskEndedMessage.java
@@ -3,5 +3,5 @@ package il.ac.technion.cs.sd.lib.clientserver;
 
 /* represents a message sent to the client, indicating that the ServerTask was 
  * completed.*/
-public class ServerTaskEndedMessage extends  DataPacket {
+public class ServerTaskEndedMessage extends  AbstractMessage {
 }
