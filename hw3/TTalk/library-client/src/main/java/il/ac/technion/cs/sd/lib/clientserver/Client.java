78509bb16859fdf3f78ecc99e12ee938787197da
Ofer Givoli <ogivoli@cs.technion.ac.il>
2015-06-05 10:01:31 +0300

    improved client interface

--- hw3/TTalk/library-client/src/main/java/il/ac/technion/cs/sd/lib/clientserver/Client.java
+++ hw3/TTalk/library-client/src/main/java/il/ac/technion/cs/sd/lib/clientserver/Client.java
@@ -1,17 +1,34 @@
 package il.ac.technion.cs.sd.lib.clientserver;
 
 import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 public class Client {
 
+	private String _address;
+	
+	/**
+	 * 
+	 * @param address The address of the client.
+	 */
 	public Client(String address)
 	{
-		//TODO
+		_address = address;
 	}
 	
-	// String - fromAddress. //TODO
-	//TODO: switch to single client logic.
-	public <T> void start(BiConsumer<T, String> callback, Class<T> type)
+	
+	/**
+	 * Starts the listen loop of the client.
+	 * During the listen loop - messages are received from the server.
+	 * Each message received from the server invokes a consumer's callback function.
+	 * @param consumer The consumer who's callback will be invoked for each message received from the server. 
+	 * @param type The type of the object the server sends the client in each message as data.
+	 * (i.e. the parameter type of the consumer's callback function).
+	 * Generic types are not supported.
+	 * @throws InvalidMessage Invalid message received from the server (the object sent as data was 
+	 * not of type 'type').
+	 */
+	public <T> void start(Consumer<T> consumer, Class<T> type)
 	{
 		 //TODO
 	}
@@ -31,5 +48,6 @@ public class Client {
 		//TODO
 		return null;
 	}
-	
+
+	public class InvalidMessage extends RuntimeException {private static final long serialVersionUID = 1L;}
 }
