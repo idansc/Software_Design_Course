3b6c45d8fbcdb8bc75b1446235dddedfa0df9a9c
Ofer Givoli <ogivoli@cs.technion.ac.il>
2015-06-07 00:48:16 +0300

    InnerMessage is inner again

--- hw3/TTalk/library-common/src/main/java/il/ac/technion/cs/sd/lib/clientserver/ReliableHost.java
+++ hw3/TTalk/library-common/src/main/java/il/ac/technion/cs/sd/lib/clientserver/ReliableHost.java
@@ -79,44 +79,37 @@ class ReliableHost {
 	/**
 	 * We'll send objects of this class via Messenger.
 	 */
-	//TODO
-//	private class InnerMessage
-//	{
-//		@SuppressWarnings("unused")
-//		InnerMessage() {}
-//		InnerMessage(long messageId, Long respnseTargetId, String data, String fromAddress) {
-//			this.messageId = messageId;
-//			this.responseTargetId = respnseTargetId;
-//			this.data = data;
-//			this.fromAddress = fromAddress;
-//		}
-//
-//		Long messageId;
-//		
-//		/* The id of the message that this message is the response to, 
-//		 * or null if this message is not a response. */
-//		Long responseTargetId; 
-//		
-//		String data;
-//		
-//		// The address of the sender.
-//		String fromAddress;
-//		
-//		
-//		@Override
-//		public String toString()
-//		{
-//			return "[from:" + showable(fromAddress) + ",messageId=" + messageId + 
-//			"," + "responseTargetId=" + responseTargetId + "]"; 
-//		}
-//		
-//		//TODO:DELETE
-//		public String TMP__toString()
-//		{
-//			return "[from:" + showable(fromAddress) + ",messageId=" + messageId + 
-//			"," + "responseTargetId=" + responseTargetId + "]"; 
-//		}
-//	}
+	private class InnerMessage
+	{
+		@SuppressWarnings("unused")
+		InnerMessage() {}
+		InnerMessage(long messageId, Long respnseTargetId, String data, String fromAddress) {
+			this.messageId = messageId;
+			this.responseTargetId = respnseTargetId;
+			this.data = data;
+			this.fromAddress = fromAddress;
+		}
+
+		Long messageId;
+		
+		/* The id of the message that this message is the response to, 
+		 * or null if this message is not a response. */
+		Long responseTargetId; 
+		
+		String data;
+		
+		// The address of the sender.
+		String fromAddress;
+		
+		
+		@Override
+		public String toString()
+		{
+			return "[from:" + Utils.showable(fromAddress) + ",messageId=" + messageId + 
+			"," + "responseTargetId=" + responseTargetId + "]"; 
+		}
+
+	}
 
 	/**
 	 * @param address The address of the new host.
